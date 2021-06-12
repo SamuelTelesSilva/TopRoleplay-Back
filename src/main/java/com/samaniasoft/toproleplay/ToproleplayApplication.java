beb46191b507ca93e22cdcc0e1b1e200011e4c5b
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-06-12 14:53:40 -0300

    Projeto criado mudando a versão do jav

---
+++ src/main/java/com/samaniasoft/toproleplay/ToproleplayApplication.java
@@ -0,0 +1,13 @@
+package com.samaniasoft.toproleplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToproleplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToproleplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samaniasoft/toproleplay/ToproleplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samaniasoft.toproleplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToproleplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
